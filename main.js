0f8e5b0c5ad46fdb4a1cf0137d0abea7f0401e85
Tarcio Teles <tarcioapps7@gmail.com>
2023-07-07 20:53:07 -0300

    add

---
+++ main.js
@@ -0,0 +1,43 @@
+/*
+//Com JS puro!!!
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('buscar').addEventListener('click', function() {
+        const xhttp = new XMLHttpRequest();
+
+        const cep = document.getElementById('cep').value;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`
+
+        xhttp.open('GET', endpoint);
+        xhttp.send();
+
+        //Endereço que iremos chamar para o CEP: https://viacep.com.br/ws/2156151561/json
+    })
+})
+
+*/
+
+//Com Jquery
+$(document).ready(function() {
+    $('#buscar').click(function() {
+        const cep = $('#cep').val();
+        const endpoint = `https://viacep.com.br/ws/${cep}/json`;
+
+        $.ajax(endpoint).done(function(respostas) {
+            const logradouro = respostas.logradouro;
+            const bairro = respostas.bairro;
+            const cidade = respostas.localidade;
+            const estado = respostas.uf;
+            const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+
+            $('#endereco').val(endereco)
+
+
+
+
+        })
+    })
+})
+
+
+
+
